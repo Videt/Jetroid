5b2b886639e938bf6a0901c6a6d2256a3b189907
Videt <egor310501@mail.ru>
2021-03-27 02:49:39 +1100

    added the jetpack functionality

--- Jetroid/Assets/Jetroid/Scripts/Player.cs
+++ Jetroid/Assets/Jetroid/Scripts/Player.cs
@@ -6,6 +6,10 @@ public class Player : MonoBehaviour
 {
     public float speed = 150f;
     public Vector2 maxVelocity = new Vector2(60, 100);
+    public float jetSpeed = 200f;
+    public bool standing;
+    public float standingThreshhold = 4f;
+    public float airSpeedMultiplier = 0.3f;
 
     private Rigidbody2D body2D;
     private SpriteRenderer renderer2D;
@@ -21,6 +25,16 @@ public class Player : MonoBehaviour
     void Update()
     {
         float absVelX = Mathf.Abs(body2D.velocity.x);
+        float absVelY = Mathf.Abs(body2D.velocity.y);
+
+        if (absVelY <= standingThreshhold)
+        {
+            standing = true;
+        }
+        else
+        {
+            standing = false;
+        }
 
         float forceX = 0f;
         float forceY = 0f;
@@ -29,7 +43,7 @@ public class Player : MonoBehaviour
         {
             if(absVelX < maxVelocity.x)
             {
-                forceX = speed;
+                forceX = standing ? speed : (speed * airSpeedMultiplier);
             }
 
             renderer2D.flipX = false;
@@ -38,10 +52,18 @@ public class Player : MonoBehaviour
         {
             if (absVelX < maxVelocity.x)
             {
-                forceX = -speed;
+                forceX = standing ? -speed : -(speed * airSpeedMultiplier);
             }
 
             renderer2D.flipX = true;
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            if (absVelY < maxVelocity.y)
+            {
+                forceY = jetSpeed;
+            }
         }
 
         body2D.AddForce(new Vector2(forceX, forceY));
