cf2c4017bf704143d482f6bee8b24d97414d39d2
Videt <egor310501@mail.ru>
2021-03-30 10:50:39 +1100

    animated the splash screen elements

---
+++ Jetroid/Assets/Jetroid/Scripts/FloatEffect.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatEffect : MonoBehaviour
+{
+    private float startY = 0f;
+    private float duration = 1f;
+    private RectTransform rectTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        startY = rectTransform.anchoredPosition.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newY = startY + (startY + Mathf.Cos(Time.time / duration * 2)) / 0.1f;
+        rectTransform.anchoredPosition = new Vector2(rectTransform.anchoredPosition.x, newY);
+    }
+}
--- Jetroid/Assets/Jetroid/Scripts/LookForward.cs
+++ Jetroid/Assets/Jetroid/Scripts/LookForward.cs
@@ -10,12 +10,6 @@ public class LookForward : MonoBehaviour
 
     private bool collision;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
